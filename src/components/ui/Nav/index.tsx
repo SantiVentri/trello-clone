6befae69673eb47587210469e43921cd0f6d031c
Santino Ventrice <santino.ventrice@gmail.com>
2025-11-26 14:05:44 -0300

    Feat: Adding settings modal in Nav component

--- src/components/ui/Nav/index.tsx
+++ src/components/ui/Nav/index.tsx
@@ -1,19 +1,40 @@
+'use client'
+
 // Styles
+import { Ellipsis, Minus, X } from "lucide-react";
 import styles from "./Nav.module.css";
 
 // Components
-import Link from "next/link";
+import { useState } from "react";
 
 export default function Nav() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <header className={styles.nav}>
             <h1>My Trello</h1>
             <nav>
-                <Link href="https://github.com/SantiVentri/trello-clone" target="_blank" className={styles.link}>See Repository</Link>
-                <Link href="https://github.com/SantiVentri/trello-clone" target="_blank" className={styles.github}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
-                </Link>
+                <button className={styles.settings} onClick={() => setIsMenuOpen(true)}>
+                    <Ellipsis color="white" />
+                </button>
             </nav>
+            <div className={`${styles.modal} ${isMenuOpen ? styles.open : styles.closed}`}>
+                <div className={styles.modalHeader}>
+                    <h4>Menú</h4>
+                    <button onClick={() => setIsMenuOpen(false)}>
+                        <X color="#4a4a4a" size={18} />
+                    </button>
+                </div>
+                <div className={styles.modalContent}>
+                    <button>
+                        <Minus color="black" size={16} />
+                        Change background
+                    </button>
+                    <button>
+                        <Minus color="black" size={16} />
+                        Close board
+                    </button>
+                </div>
+            </div>
         </header>
     )
 }
