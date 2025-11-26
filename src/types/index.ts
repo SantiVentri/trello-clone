696d043339b35eb0596d8903ff77902b4d6a83c4
Santino Ventrice <santino.ventrice@gmail.com>
2025-11-26 02:32:11 -0300

    Feat: Adding Types for Board, List, and Card

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,18 @@
 // En este archivo se definen los tipos e interfaces de Board, List y Card
+
+export type Id = string | number;
+
+export type Card = {
+  id: Id;
+  title: string;
+};
+
+export type List = {
+  id: Id;
+  title: string;
+  cards: Card[];
+};
+
+export type BoardData = {
+  lists: List[];
+};
