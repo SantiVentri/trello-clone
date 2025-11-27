e106bc506662b64889a4904be3d1449247e29c69
Santino Ventrice <santino.ventrice@gmail.com>
2025-11-26 21:33:30 -0300

    Refactor: Updating the Nav menu
    Adding GitHub link, adding icons and adding color red to close board button

--- src/components/ui/Nav/index.tsx
+++ src/components/ui/Nav/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 // Styles
-import { Ellipsis, Minus, X } from "lucide-react";
+import { Ellipsis, ImageIcon, Minus, Trash2, X } from "lucide-react";
 import styles from "./Nav.module.css";
 
 // Components
@@ -12,6 +12,8 @@ import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 // Data
 import { initialData } from "@/data/initialData";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Nav() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -85,19 +87,28 @@ export default function Nav() {
             </nav>
             <div className={`${styles.modal} ${isMenuOpen ? styles.open : styles.closed}`}>
                 <div className={styles.modalHeader}>
-                    <h4>Menú</h4>
+                    <h4>Menu</h4>
                     <button onClick={() => setIsMenuOpen(false)}>
                         <X color="#4a4a4a" size={18} />
                     </button>
                 </div>
                 <div className={styles.modalContent}>
+                    <Link href="https://github.com/santiventri/trello-clone" target="_blank">
+                        <Image
+                            src={require("../../../../public/icons/GitHub.png")}
+                            height={20}
+                            width={20}
+                            alt="Github Icon"
+                        />
+                        <p>See repository</p>
+                    </Link>
                     <button>
-                        <Minus color="black" size={16} />
-                        Change background
+                        <ImageIcon color="black" size={16} />
+                        <p>Change background (In development)</p>
                     </button>
-                    <button onClick={handleCloseBoard}>
-                        <Minus color="black" size={16} />
-                        Close board
+                    <button onClick={handleCloseBoard} style={{ color: "red" }}>
+                        <Trash2 size={16} />
+                        <p>Close board</p>
                     </button>
                 </div>
             </div>
